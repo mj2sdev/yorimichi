7e95104f33d0e70ecfa70e39b2d445c2363eee6c
3ll3702 <3ll37023@gmail.com>
2025-09-08 19:26:54 +0900

    mod(name): timeline -> station

--- src/main/java/com/jslhrd/yorimichi/controller/StationController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/StationController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/timeline")
-public class TimelineController {
+@RequestMapping("/station")
+public class StationController {
 
     @GetMapping("/list")
     public void toList () {
