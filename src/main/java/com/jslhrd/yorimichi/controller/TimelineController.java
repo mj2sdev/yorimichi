c1af433ccd8f301db58c1731cbce25590afca745
3ll3702 <3ll37023@gmail.com>
2025-09-08 18:19:07 +0900

    add(front): timeline feature added two files created. timeline/list.html TimelineController.java

---
+++ src/main/java/com/jslhrd/yorimichi/controller/TimelineController.java
@@ -0,0 +1,16 @@
+package com.jslhrd.yorimichi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequestMapping("/timeline")
+public class TimelineController {
+
+    @GetMapping("/list")
+    public void toList () {
+    }
+    
+    
+}
