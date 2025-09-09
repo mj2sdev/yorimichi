58753d50a31e01a212d9df950c2adf1d536b6649
Isha-dev <isha1114un@gmail.com>
2025-09-09 15:09:02 +0900

    add(front):어드민 대쉬보드 임시

---
+++ src/main/java/com/jslhrd/yorimichi/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.jslhrd.yorimichi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    
+    @GetMapping("/dashboard")
+    public void toDashBoard() {
+        
+    }
+    
+
+}
