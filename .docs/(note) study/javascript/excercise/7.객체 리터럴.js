3b3a0791cd61f741cc35c745cf101c32eb4e55de
3ll3702 <3ll37023@gmail.com>
2025-09-14 15:22:55 +0900

    docs: 자바스크립트 객체 부분 추가

--- .docs/(note) study/javascript/excercise/7.객체 리터럴.js
+++ .docs/(note) study/javascript/excercise/7.객체 리터럴.js
@@ -5,3 +5,15 @@ obj[key] = 'world';
 //var obj = { [key]: 'world'};
 
 console.log(obj); // {hello: 'world'}
+
+
+
+var prefix = 'prop';
+var i=0;
+var obj={};
+
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+obj[prefix + '-' + ++i] = i;
+
+console.log(obj); //{prop-1: 1, prop-2: 2,prop-3: 3}
