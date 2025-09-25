bb2cea0375d804c1093435b149a9f4ddf2161778
mj2sdev <mj2sdev@gmail.com>
2025-09-25 23:00:20 +0900

    fix(controller): 매핑실패로 서버 오픈 안되는 사항 수정

--- src/main/java/com/jslhrd/yorimichi/controller/StoreController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/StoreController.java
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class StoreController {
 
 	//첫검색 혹은 검색 버튼을 눌러서 검색.
-	@GetMapping("/search?keyword={keyword}&tags={tags}")
+	// @GetMapping("/search?keyword={keyword}&tags={tags}")
+	@GetMapping("/search")
 	public String showStoresBySearch() {
 		//List<StoreDTO> stores
 		//List<StoreCategoryDTO> categories
@@ -29,7 +30,8 @@ public class StoreController {
 	//비동기로 리스트를 가져오기 위한 메서드
 	//카테고리나 시설 등 버튼을 눌러 설정이 바뀌면 리스트가 달라지는 형태.
 	@ResponseBody
-	@GetMapping("/search?keyword={keyword}&tags={tags}")
+	// @GetMapping("/search?keyword={keyword}&tags={tags}")
+	@GetMapping("/search2")
 	public void showStoresByFilter() {
 		
 		//서비스로 가서 키워드로 가져온 가게 리스를 보여줄 수 있어야 함.
