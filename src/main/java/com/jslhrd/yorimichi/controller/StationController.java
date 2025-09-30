a4c9da7ec9a7cd580440546ef66c165143937e6c
3ll3702 <3ll37023@gmail.com>
2025-10-01 01:45:10 +0900

    fix(controller) /station 요청을 받는 Controller를 변경.

--- src/main/java/com/jslhrd/yorimichi/controller/StationController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/StationController.java
@@ -14,6 +14,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class StationController {
     
     //private final StationService stationService
+
+    //정류장으로 이동
+	@GetMapping("/station")
+	public String showStation() {
+		//사용자의 id를 바탕으로 팔로우, 팔로워 등을 가져올 수 있어야 함
+		//같이먹기나와 리뷰의 리스트를 가져올 수 있어야함
+		return "station/list";
+	}
     
     //최신 리뷰를 가져오는 피드, count로 몇개 받아올 건지 정함
     @GetMapping("/feed")
