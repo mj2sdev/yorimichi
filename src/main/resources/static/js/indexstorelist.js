b6fa9e00e1216ad4ebb06c0953b9d330e4aaf1ea
3ll3702 <3ll37023@gmail.com>
2025-09-30 11:48:32 +0900

    mod(index): js를 통해서 비동기통신을 만들어봤습니다.

---
+++ src/main/resources/static/js/indexstorelist.js
@@ -0,0 +1,62 @@
+const state = {
+	stores: []
+}
+
+function render() {
+	const storeListContainer = document.querySelector('.storeList');
+	const storeTemplate = document.getElementById('.storelist-template');
+
+	storeListContainer.innerHTML = '';
+	
+	state.stores.forEach(store => {
+		const clone = document.importNode(storeItemTemplate.content, true);
+		const link = clone.querySelector('a');
+		link.href = `/store/detail/${store.id}`;
+		clone.querySelector('.store-name').textContent = store.storeName;
+        clone.querySelector('.store-category').textContent = store.category;
+        clone.querySelector('.store-address').textContent = store.address;
+        clone.querySelector('.store-rating-number').textContent = `${store.rating}점`;
+        clone.querySelector('.store-reviews-count').textContent = store.reviewCount;
+
+		const starContainer = clone.querySelector('.store-rating-star');
+		starContainer.innerHTML = generateStars(store.rating);
+		storeListContainer.appendChild(clone);
+	});
+}
+
+function generateStars(rating) {
+    let starsHTML = '';
+	const roundedRating = Math.round(rating);
+	for (let i = 1; i <= 5; i++) {
+        if (i <= roundedRating) {
+            starsHTML += '<i class="bi bi-star-fill" style="font-size: 1em; color: gold;"></i>';
+        } else {
+            starsHTML += '<i class="bi bi-star" style="font-size: 1em; color: lightgray;"></i>';
+        }
+    }
+    return starsHTML;
+}
+
+async function fetchStores() {
+	
+	try{
+		const response = await fetch('/search');
+	
+		if(!response.ok){
+			throw new error('HTTP error! status: '+ response.status);
+		}
+
+		const newStores = await response.json();
+
+		state.stores = newStores;
+
+		render();
+	} catch (error){
+		console.error("가게 목록 불러오기 실패.", error);
+	}
+}
+
+documnet.addEventListener('DOMContentLoaded', () => {
+	fetchStores();
+	setInterval(fetchStores, 30000);
+})
