4a4d2c924446d08c7bc517f7708c077e41a19f02
3ll3702 <3ll37023@gmail.com>
2025-10-01 01:45:10 +0900

    fix(controller) /station 요청을 받는 Controller를 변경.

--- src/main/java/com/jslhrd/yorimichi/controller/PageController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/PageController.java
@@ -21,13 +21,4 @@ public class PageController {
 		//음식 카테고리를 가져와야함
 		return "index";
 	}
-
-	//정류장으로 이동
-	@GetMapping("/station")
-	public String showStation() {
-		//사용자의 id를 바탕으로 팔로우, 팔로워 등을 가져올 수 있어야 함
-		//같이먹기나와 리뷰의 리스트를 가져올 수 있어야함
-		return "station/list";
-	}
-
 }
