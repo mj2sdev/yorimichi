c3391a15552503911f41410d34f3fe2592790700
3ll3702 <3ll37023@gmail.com>
2025-10-01 20:07:11 +0900

    mod(script): 오타를 수정했습니다.

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -9,89 +9,85 @@ const request = {
         this.requestUrl = endpoint;
         this.requestUrl += '?' + new URLSearchParams(data).toString();
         this.fetchOption.method = 'GET';
-        this.send();
+        return this.send();
     },
 
     delete: function (endpoint, data){
         this.requestUrl = endpoint;
         this.requestUrl += '?' + new URLSearchParams(data).toString();
         this.fetchOption.method = 'DELETE';
-        this.send();
+        return this.send();
     },
 
     post: function (endpoint, data){
         this.requestUrl = endpoint;
         this.fetchOption.method = 'POST';
         if(data instanceof HTMLFormElement){
-            this.fetchOption.headers = {
-                'Contetn-Type': 'multipart/form-data'
-            };
             this.fetchOption.body = new FormData(data);
         }else if(data&&data.constructor === Object){
             this.fetchOption.headers = {
                 'Content-Type': 'application/json'
             };
-            this.sfetchOption.body = JSON.stringify(data);
+            this.fetchOption.body = JSON.stringify(data);
         }
-        this.send();
+        return this.send();
     },
 
     put: function (endpoint, data){
         this.requestUrl = endpoint;
         this.fetchOption.method = 'PUT';
         if(data instanceof HTMLFormElement){
-            this.fetchOption.headers = {
-                'Contetn-Type': 'multipart/form-data'
-            };
             this.fetchOption.body = new FormData(data);
         }else if(data&&data.constructor === Object){
             this.fetchOption.headers = {
                 'Content-Type': 'application/json'
             };
-            this.sfetchOption.body = JSON.stringify(data);
+            this.fetchOption.body = JSON.stringify(data);
         }
-        this.send();
+        return this.send();
     },
 
     patch: function (endpoint, data){
         this.requestUrl = endpoint;
         this.fetchOption.method = 'PATCH';
         if(data instanceof HTMLFormElement){
-            this.fetchOption.headers = {
-                'Contetn-Type': 'multipart/form-data'
-            };
             this.fetchOption.body = new FormData(data);
         }else if(data&&data.constructor === Object){
             this.fetchOption.headers = {
                 'Content-Type': 'application/json'
             };
-            this.sfetchOption.body = JSON.stringify(data);
+            this.fetchOption.body = JSON.stringify(data);
         }
-        this.send();
+        return this.send();
     },
 
     send: async function (){
+        const url = this.requestUrl;
+        const options = this.fetchOption;
+
+        this.clear();
+
         try{
-            const response = await fetch(this.requestUrl, this.fetchOption)
+            const response = await fetch(url, options)
             if(!response.ok){
                 throw new Error(response.status);
             }
             if(response.status === 204){
-                this.clear;
+
                 return true;
             }
-            this.clear;
             return await response.json();     
         } catch (error) {
             console.error('데이터 fetch 실패',error);
-            this.clear;
             return null;
         }        
     },
 
     clear: function(){
-        this.requestUrl = null;
-        this.fetchOption = {};
+        this.requestUrl = '';
+        this.fetchOption = {
+             method: '',
+        };
     }
 }
 request.clear();
