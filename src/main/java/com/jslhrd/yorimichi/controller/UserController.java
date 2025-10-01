e0eaaad8fb609bfdb3e495be6d2c1749d4668585
mj2sdev <93305687+mj2sdev@users.noreply.github.com>
2025-10-01 11:35:57 +0900

    Update UserController.java

--- src/main/java/com/jslhrd/yorimichi/controller/UserController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/UserController.java
@@ -54,8 +54,19 @@ public class UserController {
 	//개인정보 보호 설정
 	@ResponseBody
 	@PutMapping("/mypage/privacy")
-	public void putMethodName() {
+	public void putMethodName(@RequestBody UserDTO dto) {
 		//TODO:얘 어떻게 처리하면 좋을지 모르겠습니다. 일단 버튼 딸깍식으로 있긴 한데. 도움 요청합니다.
+		/*
+		바로 위에 putMapping("mypage") << 여기랑 비슷하게 처리하시면 됩니다. 
+		dto 에는 내용이 추가되어야 합니다.
+		dto
+		- hiddenReview (Boolean)
+		- hiddenLike (Boolean)
+		- hiddenFavorite (Boolean)
+		- hiddenFollow (Boolean)
+		- hiddenFollower (Boolean)
+		userService.updatePrivacy(dto)
+		*/
 	}
 
 
