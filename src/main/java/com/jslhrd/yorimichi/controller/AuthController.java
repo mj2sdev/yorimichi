72d0e3501fc40e57ff8303c11f53b9eb08be4416
3ll3702 <3ll37023@gmail.com>
2025-10-02 14:35:45 +0900

    fix(endpoint): -를 지우고 /로 변경

--- src/main/java/com/jslhrd/yorimichi/controller/AuthController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/AuthController.java
@@ -55,7 +55,7 @@ public class AuthController {
 
 	//이메일 인증
 	@ResponseBody
-	@PostMapping("/signup/email-verification")
+	@PostMapping("/signup/email/verification")
 	public boolean verificateEmail(@RequestBody String email) {
 		return accountService.verificateEmail(email);
 	}
