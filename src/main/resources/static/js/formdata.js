baa1b13681b26e622d1132298729892cab040e3f
mj2sdev <93305687+mj2sdev@users.noreply.github.com>
2025-10-02 14:11:52 +0900

    Update formdata.js

--- src/main/resources/static/js/formdata.js
+++ src/main/resources/static/js/formdata.js
@@ -1,9 +1,10 @@
 /**
  * 
- * @param {formId} form 
- * @returns multipart/form-data
+ * @param {string} formId
+ * @returns {FormData}
+ * 리턴타입은 FormData 이며 multipart/form-data 방식으로 전송하기 위해 사용됩니다. 
  */
-function formData(form){
+function formData(formId){
     const formdata = document.getElementById(form);
     return new FormData(formdata);
 };
