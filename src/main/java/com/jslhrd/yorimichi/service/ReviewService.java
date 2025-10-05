953e49bc657bf84029c3363073f4288bcf2eb6ad
mj2sdev <mj2sdev@gmail.com>
2025-10-05 10:44:45 +0900

    fix(service): ReviewService 주석 변경

--- src/main/java/com/jslhrd/yorimichi/service/ReviewService.java
+++ src/main/java/com/jslhrd/yorimichi/service/ReviewService.java
@@ -9,29 +9,32 @@ import java.util.List;
  * 리뷰 관련 서비스 인터페이스 입니다.
  *
  * @author mj2sdev
- * @since 1.0
+ * @version 1.0 초안 작성
+ * 
+ * @author LancerAlert
+ * @version 1.1 실제 설계도에 맞게 변경
  */
 public interface ReviewService {
 
 	/**
 	 * 최신 리뷰 리스트를 조회합니다.
 	 *
-	 * @return review list
+	 * @return {@code List<ReviewDTO>} 리뷰 LIST
 	 */
 	public List<ReviewDTO> findAll();
 
 	/**
-	 * 가게 아이디를 기준으로 리뷰 리스트를 조회합니다.
+	 * 가게 ID를 기반으로 리뷰를 여러 개 찾습니다.
 	 *
-	 * @param storeId 가게 아이디
-	 * @return review list
+	 * @param  storeId 가게 ID
+	 * @return {@code List<ReviewDTO>} 리뷰 LIST
 	 */
 	public List<ReviewDTO> findAllByStoreId(Long storeId);
 
 	/**
 	 * 리뷰 상세 데이터를 조회합니다.
 	 *
-	 * @param reviewId 리뷰 아이디
+	 * @param  reviewId 리뷰 ID
 	 * @return {@code ReviewDTO} 리뷰 데이터
 	 */
 	public ReviewDTO findById(Long reviewId);
@@ -66,11 +69,9 @@ public interface ReviewService {
 
 	/**
 	 * 리뷰아이디 (reviewId) 를 이용해 리뷰를 삭제합니다.
-	 * TODO: 컨트롤러의 임시 작성을 위해 principal을 추가했습니다. 검토 바랍니다.
 	 *
 	 * @param userId   권한 확인 유저 ID
 	 * @param reviewId 삭제할 리뷰 ID
 	 */
 	public void delete(Long userId, Long reviewId);
-
 }
