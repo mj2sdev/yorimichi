4150d3fab9cb5429b8073e0e9d421f2323138d11
LancerAlert <tmfcks20@gmail.com>
2025-10-05 17:58:22 +0900

    mod(store): id, dto, affected 등 명확하게 수정. - `id` -> `storeId` - `dto` -> `store` - `rootAffected` -> `affectedRoot` - `storeAffected` -> `affectedStore` - affected 의 타입을 `int` -> `boolean`

--- src/main/java/com/jslhrd/yorimichi/exception/StoreNotFoundException.java
+++ src/main/java/com/jslhrd/yorimichi/exception/StoreNotFoundException.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class StoreNotFoundException extends RuntimeException {
-	
-	public StoreNotFoundException(Long id) {
-		super("Store not found: " + id);
+
+	public StoreNotFoundException(Long storeId) {
+		super("Store not found: " + storeId);
 	}
 }
