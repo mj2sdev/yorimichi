06e49f2ef47ed11976dd53b30fd3ae2ceb7dd7c8
3ll3702 <3ll37023@gmail.com>
2025-10-07 14:55:01 +0900

    mod(relationship): rebase develop

---
+++ src/main/resources/static/js/user/mypage/myinfo.js
@@ -0,0 +1,15 @@
+(function (){
+     const submitBtn = document.getElementById('myinfosubmit');
+     
+
+     submitBtn.addEventListener('click', async function() {
+        const data = formData('myinfo');
+        const response = await request('PUT', '/mypage', {body: data});
+        if(response){
+            alert('정보 수정 성공');
+            window.location.reload();
+        }else{
+            alert('정보 수정 실패');
+        }
+    });
+})();
