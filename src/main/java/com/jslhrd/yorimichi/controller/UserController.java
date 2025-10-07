93abd8cf2b86aa2b8afc7f55659f1184832bb095
3ll3702 <3ll37023@gmail.com>
2025-10-07 14:55:01 +0900

    mod(fornt):  html수정, js추가, controller, service, dto 수정

--- src/main/java/com/jslhrd/yorimichi/controller/UserController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/UserController.java
@@ -1,21 +1,29 @@
 package com.jslhrd.yorimichi.controller;
 
 import java.security.Principal;
+import java.util.List;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.jslhrd.yorimichi.domain.BookmarkDTO;
+import com.jslhrd.yorimichi.domain.ReviewDTO;
+import com.jslhrd.yorimichi.domain.StoreDTO;
 import com.jslhrd.yorimichi.domain.UserDTO;
+import com.jslhrd.yorimichi.service.ReviewService;
+import com.jslhrd.yorimichi.service.StoreService;
 import com.jslhrd.yorimichi.service.UserService;
+import com.jslhrd.yorimichi.service.RelationshipService;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -27,12 +35,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class UserController {
 
 	private final UserService userService;
-	
+	private final StoreService storeService;
+	private final ReviewService reviewService;
+	private final RelationshipService relationshipService;
+
 	//유저 상세페이지 이동
 	@GetMapping("/detail/{id}")
 	public String showUserDetail(@PathVariable("id") Long userId, Model model) {
 		UserDTO user = userService.findById(userId);
+		List<ReviewDTO> reviews = reviewService.findAllByUserId(null);
+		List<StoreDTO> likes = storeService.findAllByUserLike(null);
+		List<BookmarkDTO> bookmarks = null;
+		List<UserDTO> follow = relationshipService.findFollowById(null);
+		List<UserDTO> follower = relationshipService.findFollowerById(null);
+		List<UserDTO> block = relationshipService.findBlockById(null);
 		model.addAttribute("user", user);
+		model.addAttribute("reviews", reviews);
+		model.addAttribute("likes", likes);
+		model.addAttribute("bookmarks", bookmarks);
+		model.addAttribute("follow", follow);
+		model.addAttribute("follower", follower);
+		model.addAttribute("block", block);
 		return "user/detail";
 	}
 
@@ -40,37 +63,26 @@ public class UserController {
 	@GetMapping("/mypage")
 	public String showMypage(Principal principal, Model model) {
 		UserDTO user = userService.findById(null);
+		List<ReviewDTO> reviews = reviewService.findAllByUserId(null);
+		List<StoreDTO> likes = storeService.findAllByUserLike(null);
+		List<BookmarkDTO> bookmarks = null;
+		List<UserDTO> follow = relationshipService.findFollowById(null);
+		List<UserDTO> follower = relationshipService.findFollowerById(null);
+		List<UserDTO> block = relationshipService.findBlockById(null);
 		model.addAttribute("user", user);
+		model.addAttribute("reviews", reviews);
+		model.addAttribute("likes", likes);
+		model.addAttribute("bookmarks", bookmarks);
+		model.addAttribute("follow", follow);
+		model.addAttribute("follower", follower);
+		model.addAttribute("block", block);
 		return "user/mypage";
 	}
 
 	//자기 정보 수정
 	@ResponseBody
 	@PutMapping("/mypage")
-	public void updateMyDetail(@RequestBody UserDTO user) {
+	public void updateMyDetail(Principal principal, @ModelAttribute UserDTO user) {
 		userService.update(user);
 	}
-
-	//개인정보 보호 설정
-	@ResponseBody
-	@PutMapping("/mypage/privacy")
-	public void putMethodName(@RequestBody UserDTO dto) {
-		//TODO:얘 어떻게 처리하면 좋을지 모르겠습니다. 일단 버튼 딸깍식으로 있긴 한데. 도움 요청합니다.
-		/*
-		바로 위에 putMapping("mypage") << 여기랑 비슷하게 처리하시면 됩니다. 
-		dto 에는 내용이 추가되어야 합니다.
-		dto
-		- hiddenReview (Boolean)
-		- hiddenLike (Boolean)
-		- hiddenFavorite (Boolean)
-		- hiddenFollow (Boolean)
-		- hiddenFollower (Boolean)
-		userService.updatePrivacy(dto)
-		*/
-	}
-
-
-	
-	
-	
 }
