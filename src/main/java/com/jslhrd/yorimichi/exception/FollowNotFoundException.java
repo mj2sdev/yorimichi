9009976db9958f063e683d5a24c783df6d9f81eb
LancerAlert <tmfcks20@gmail.com>
2025-10-08 14:42:50 +0900

    mod(relationship): 예외 수정 및 제거.

--- src/main/java/com/jslhrd/yorimichi/exception/FollowNotFoundException.java
+++ src/main/java/com/jslhrd/yorimichi/exception/FollowNotFoundException.java
@@ -3,10 +3,19 @@ package com.jslhrd.yorimichi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Map;
+
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class FollowNotFoundException extends RuntimeException {
+public class FollowNotFoundException extends DomainException {
 
 	public FollowNotFoundException(Long followerId, Long followeeId) {
-		super("Follow not found follower id " + followerId + " and followee id " + followeeId);
+		super(
+				"FOLLOW_NOT_FOUND",
+				"팔로우를 찾을 수 없습니다.",
+				Map.of(
+						"followerId", followerId,
+						"followeeId", followeeId
+				)
+		);
 	}
 }
