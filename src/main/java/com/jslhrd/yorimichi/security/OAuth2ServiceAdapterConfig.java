71fcecdf2182e436e4fa7fb5f4f6ec809de4c764
LancerAlert <pquy0176@naver.com>
2025-10-14 10:07:51 +0900

    mod(security): 중복 클래스 제거.

--- src/main/java/com/jslhrd/yorimichi/security/OAuth2ServiceAdapterConfig.java
+++ src/main/java/com/jslhrd/yorimichi/security/OAuth2ServiceAdapterConfig.java
@@ -1,12 +1,14 @@
 package com.jslhrd.yorimichi.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+@Configuration
 public class OAuth2ServiceAdapterConfig {
 
 	@Bean
