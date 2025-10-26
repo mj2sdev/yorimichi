f5f32de246c34b0c8313a9f1a3eaf3637aeff50c
mj2sdev <mj2sdev@gmail.com>
2025-10-26 15:25:59 +0900

    mod(controller): review 컨트롤러 리뷰작성 endPoint stores -> store 로 수정

--- src/main/java/com/jslhrd/yorimichi/controller/ReviewController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/ReviewController.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -43,12 +44,11 @@ public class ReviewController {
 
 
 	// 리뷰작성
-	@PostMapping("/stores/{storeId}/review")
+	@PostMapping("/store/{storeId}/review")
 	public void submitReview(
 			@PathVariable("storeId") Long storeId,
 			@AuthenticationPrincipal(expression = "userId") Long userId,
-			@RequestBody ReviewDTO dto
-	) {
+			@ModelAttribute ReviewDTO dto) {
 		reviewService.save(userId, storeId, dto);
 	}
 
