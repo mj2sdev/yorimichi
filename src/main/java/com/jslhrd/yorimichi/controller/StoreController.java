de339b89630742507b2486065c7cffdd6d419ee5
mj2sdev <mj2sdev@gmail.com>
2025-10-29 23:45:05 +0900

    mod(coeat): 같이먹기에 필요한 컨트롤러 수정

--- src/main/java/com/jslhrd/yorimichi/controller/StoreController.java
+++ src/main/java/com/jslhrd/yorimichi/controller/StoreController.java
@@ -1,9 +1,9 @@
 package com.jslhrd.yorimichi.controller;
 
-import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
+import com.jslhrd.yorimichi.domain.CoeatDTO;
 import com.jslhrd.yorimichi.service.ApiKeyService;
 import com.jslhrd.yorimichi.service.BookmarkService;
 import com.jslhrd.yorimichi.service.LikeService;
@@ -30,10 +30,9 @@ public class StoreController {
 
 	//인덱스 페이지에서 가게 맛집 리스트를 보여주기 위함. 인기있는 최신 6개를 보여주면 될 거 같음.
 	//인기의 기준이 뭔지 모르겠음.
-	@ResponseBody
 	@GetMapping("/search")
-	public void showStores() {
-
+	public String showStores() {
+		return "store/list";
 	}
 
 
@@ -70,6 +69,7 @@ public class StoreController {
 		@PathVariable("storeId") Long storeId,
 		@AuthenticationPrincipal(expression = "userId") Long userId,
 		Model model) {
+		model.addAttribute("coeatDto", new CoeatDTO());
 		model.addAttribute("store", storeService.findById(storeId, userId));
 		model.addAttribute("mapKey", apiKeyService.findApiKey("maps", null));
 		return "store/detail";
